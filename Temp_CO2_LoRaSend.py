0d2124e37ab6f4c3e45045c450fa6bdfcfb4de00
Émilie Saucier <esaucier@buffalo.edu>
2025-03-07 15:32:12 -0500

    Update Temp_CO2_LoRaSend.py

--- Temp_CO2_LoRaSend.py
+++ Temp_CO2_LoRaSend.py
@@ -6,19 +6,18 @@ import adafruit_rfm9x
 import adafruit_mcp9600
 import adafruit_scd4x
 
+# set the time interval (seconds) for sending packets
+transmit_interval = 1
+
 # Initate time count
 elapsed_sec = 0
 
 ######### CO2 #########
 #Gas readout
-""" Example for using the SGP30 with CircuitPython and the Adafruit library"""
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = board.I2C()  # uses board.SCL and board.SDA
+scd4x = adafruit_scd4x.SCD4X(i2c)
 
-# Create library object on our I2C port
-sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
-
-sgp30.set_iaq_baseline(0x8973, 0x8AAE)
-sgp30.set_iaq_relative_humidity(celsius=22.1, relative_humidity=44)
+scd4x.start_periodic_measurement()
 
 ######### Temperature #########
 # Initialize I2C for MCP9600
@@ -26,9 +25,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mcp = adafruit_mcp9600.MCP9600(i2c)
 
 ###### RADIO ########
-# set the time interval (seconds) for sending packets
-transmit_interval = 1
-
 # Define radio parameters.
 RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in MHz.
 
@@ -51,8 +47,8 @@ rfm9x.tx_power = 23
 print("Sending packets...")
 while True:
     # Get temperature from MCP9600 sensor
-    print("eCO2 = %d ppm \t TVOC = %d ppb \t Temperature = %d °C" % (sgp30.eCO2, sgp30.TVOC, mcp.temperature))
+    print("CO2 = %d ppm \t Temperature = %d °C" % (scd4x.CO2, mcp.temperature))
     elapsed_sec += transmit_interval
     # send a broadcast mesage
-    rfm9x.send(bytes("time = %d seconds \t eCO2 = %d ppm \t TVOC = %d ppb \t Temperature = %d °C" % (elapsed_sec, sgp30.eCO2, sgp30.TVOC, mcp.temperature), "UTF-8"))
+    rfm9x.send(bytes("time = %d seconds \t CO2 = %d ppm \t Temperature = %d °C" % (elapsed_sec, scd4x.CO2, mcp.temperature), "UTF-8"))
     time.sleep(transmit_interval)
